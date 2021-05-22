4e3f631aac7a06a7a3f471f423e41f7117a4b3ec
wslmacieira <wslmacieira@outlook.com>
2021-05-22 11:01:14 -0300

    testando alterações

--- app/pages/room/src/index.js
+++ app/pages/room/src/index.js
@@ -3,17 +3,21 @@ import RoomController from "./controller.js"
 import RoomSocketBuilder from "./util/roomSocket.js"
 import View from "./view.js"
 
-const room = {
-    id: '0001',
-    topic: 'Js Expert éh noix'
-}
+const urlParams = new URLSearchParams(window.location.search)
+const keys = ['id', 'topic']
+
+const urlData = keys.map((key) => [key, urlParams.get(key)])
 
 const user = {
     img: 'https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/bear_russian_animal_avatar-256.png',
     username: 'wslmacieira ' + Date.now()
 }
 
-const roomInfo = { user, room }
+const roomInfo = {
+    room: {...Object.fromEntries(urlData)},
+    user
+}
+
 
 const socketBuilder = new RoomSocketBuilder({
     socketUrl: constants.sockeUrl,
